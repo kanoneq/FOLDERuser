6516f77c6cfae01616aabfdfc0df041901f07b85
kanoneq <kanoneqisgone@gmail.com>
2024-03-13 18:37:20 +0100

    language and slight changes

--- USERfolder.bat
+++ USERfolder.bat
@@ -1,139 +1,136 @@
 @echo off
 color 0a
 cls
-goto start
+goto start2
 
 :emergency start
 cls
-echo zacznijmy od nowa
-echo.
+echo you did something wrong, let's start once again
+echo press any key to continue
+set /p cont=
 goto start2
-
-:start
-echo dzien dobry uzytkowniku, tutaj mozesz stworzyc swoj katalog, lub dodac do niego plik tekstowy
-echo.
-goto start2
-
 
 :start2
 setlocal
-echo jesli chcesz utworzyc katalog, wpisz 1, jesli dodac .txt wpisz 2
-echo jesli natomiast chcesz zmienic nazwe .txt lub folderu wpisz 3
-echo pamietaj o opcji powrotu za pomoca komendy "cancel"
-set /p wybor=
-if %wybor%==1 goto :tworzenie
-if %wybor%==2 goto :dodawanie
-if %wybor%==3 goto :zmienianie
-if %wybor%==tukan goto :toucan
-echo nie wybrano kompatybilnej warosci
+cls
+echo hello there, here you can create your own folder and add a .txt to it
+echo.
+echo if you want to create a folder press 1, if u want to add a .txt to your folder press 2
+echo but if you want to change name of your folder/txt file press 3
+echo you can always go back by typing "cancel"
+set /p choice=
+if %choice%==1 goto :creating
+if %choice%==2 goto :adding
+if %choice%==3 goto :changing
+if %choice%==cancel goto :end
+if %choice%==tukan goto :toucan
+echo no compatible value provided
 goto emergency start
 endlocal
 
-:tworzenie
+:creating
 setlocal
-echo podaj nazwe katalogu ktory chcesz utworzyc
-set /p nazwa=
-if %nazwa%==cancel goto tworzenie
-mkdir ".\FolderyUserow\%nazwa%"
-echo podkatalog %nazwa% zostal utworzony w katalogu "foldery userow"
+echo type in a name for your folder
+set /p name=
+if %name%==cancel goto start2
+mkdir ".\UsersFolder\%name%"
+echo folder %name% was created in "UsersFolder"
 echo.
-echo jesli chcesz to mozesz stworzyc tez plik tekstowy w swoim nowym katalogu, jesli nie chcesz, to wpisz "nie"
-echo jesli chcesz to wpisz nazwe tego pliku
-set /p notatnik=
-if %notatnik%==cancel goto tworzenie
-if %notatnik%==nie goto koniec
-type nul>".\FolderyUserow\%nazwa%\%notatnik%.txt"
+echo you can create a .txt to your new folder, if not simply type "no"
+echo if yes, type its name
+set /p notepad=
+if %notepad%==no goto end
+type nul>".\UsersFolder\%name%\%notepad%.txt"
 echo.
-echo zostal utworzony plik tekstowy o nazwe %notatnik% w twoim katalogu %nazwa%
-goto koniec
+echo there is a notepad under name %notepad% in your folder %name%
+goto end
 endlocal
 
-:dodawanie
+:adding
 setlocal
-echo wpisz nazwe swojego katalogu
-set /p katalog=
-if %katalog%==cancel goto dodawanie
 echo.
-echo wpisz nazwe pliku tekstowego ktory chcesz zalozyc
-set /p pliczek=
-if %cancel%==cancel goto dodawanie
-type nul>".\FolderyUserow\%katalog%\%pliczek%.txt"
-if not exist ".\FolderyUserow\%katalog%" (
-nie ma takiego folderu
+echo type in name of your folder
+set /p catalog=
+if %catalog%==cancel goto start2
+if not exist ".\UsersFolder\%catalog%" (
 goto emergency start
 )
-echo polecenie zostalo pomyslnie wykonane
-goto koniec
+echo.
+echo type in name of your new notepad
+set /p file=
+if %file%==cancel goto adding
+type nul>".\UsersFolder\%catalog%\%file%.txt"
+echo operation was completed succesfully
+goto end
 endlocal
 
-:zmienianie
-setlocal
-echo chcesz zmienic nazwe folderu czy pliku txt? (1=txt  2=folder)
-set /p wybor=
-if %wybor%==1 goto tekstowy
-if %wybor%==2 goto folder
-if %wybor%==cancel goto start2
-endlocal
-
-:tekstowy
+:changing
 setlocal
 echo.
-echo podaj nazwe swojego folderu
+echo do you want to change name for text file or folder? (1=txt  2=folder)
+set /p choice=
+if %choice%==1 goto notepad
+if %choice%==2 goto folder
+if %choice%==cancel goto start2
+endlocal
+
+:notepad
+setlocal
+echo.
+echo enter the current name of your folder
 set /p folder=
-if %folder%==cancel goto zmienianie
+if %folder%==cancel goto changing
 echo.
-echo podaj nazwe pliku tekstowego na ktorym ma zostac wykonana operacja
-set /p tekstowy=
-if %tekstowy%==cancel goto tekstowy
-echo.
-echo podaj nowa nazwe dla swojego pliku tekstowego
-set /p txtN=
-if %txtN%==cancel goto tekstowy
-if not exist ".\FolderyUserow\%folder%\%tekstowy%.txt" (
-echo nie ma takiego folderu, pliku tekstowego lub obydwu
+echo provide the name of the text file that will be executed in the operation
+set /p txt=
+if %txt%==cancel goto notepad
+if not exist ".\UsersFolder\%folder%\%txt%.txt" (
 goto emergency start
 )
-ren ".\FolderyUserow\%folder%\%tekstowy%.txt" "%txtN%.txt"
-echo zmiana przebiegla pomyslnie
-goto koniec
+echo.
+echo provide new name for the .txt file
+set /p txtN=
+if %txtN%==cancel goto notepad
+ren ".\UsersFolder\%folder%\%txt%.txt" "%txtN%.txt"
+echo operations completed succesfully
+goto end
 endlocal
 
 :folder
 setlocal
 echo.
-echo podaj aktualna nazwe swojego folderu
+echo enter the current name of your folder
 set /p folder=
-if %folder%==cancel goto zmienianie
-echo.
-echo wybierz nowa nazwe dla twojego folderu
-set /p nazwa=
-if %nazwa%==cancel goto folder
-if not exist ".\FolderyUserow\%folder%" (
-echo taki folder nie istnieje
+if %folder%==cancel goto changing
+if not exist ".\UsersFolder\%folder%" (
 goto emergency start
 )
-ren ".\FolderyUserow\%folder%" "%nazwa%"
-echo operacja przebiegla pomyslnie
-goto koniec
+echo.
+echo pick a new name for your folder
+set /p name=
+if %name%==cancel goto folder
+ren ".\UsersFolder\%folder%" "%name%"
+echo operation completed succesfully
+goto end
 endlocal
 
 :toucan
 setlocal
-set /p wybor=
-if %wybor%==tukan goto letukan
+set /p choice=
+if %choice%==tukan goto letukan
 endlocal
 
 :letukan
 setlocal
-set/p wybor=
-if %wybor%==tukan goto finallboss
+set/p choice=
+if %choice%==tukan goto finallboss
 endlocal
 
 :finallboss
 start "" ".\tukan.jpg"
-goto koniec
+goto end
 
-:koniec
+:end
 echo.
-echo to wszystko, dzieki
+echo that's all, thank you
 pause>nul
